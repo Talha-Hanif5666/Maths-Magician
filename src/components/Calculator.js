89fc308fb9b0ea148509b61f2b125c1dd87e7e7a
Muhammad Talha Hanif <talhahanif5666@gmail.com>
2023-02-14 11:26:48 +0500

    Adding calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,35 +3,35 @@ import React from 'react';
 class Calculator extends React.PureComponent {
   render() {
     return (
-      <div className="container">
-        <div className="result">
+      <div className="main">
+        <div className="display">
           <input type="text" value={0} />
         </div>
-        <div className="buttons">
+        <div className="numerics">
           <button type="button">AC</button>
           <button type="button">+/-</button>
           <button type="button">%</button>
-          <button type="button" className="opreator">÷</button>
+          <button type="button" className="yellow">÷</button>
           <br />
 
           <button type="button">7</button>
           <button type="button">8</button>
           <button type="button">9</button>
-          <button type="button" className="opreator">x</button>
+          <button type="button" className="yellow">x</button>
           <br />
           <button type="button">4</button>
           <button type="button">5</button>
           <button type="button">6</button>
-          <button type="button" className="opreator">-</button>
+          <button type="button" className="yellow">-</button>
           <br />
           <button type="button">1</button>
           <button type="button">2</button>
           <button type="button">3</button>
-          <button type="button" className="opreator">+</button>
+          <button type="button" className="yellow">+</button>
           <br />
           <button type="button" className="zero"> 0 </button>
           <button type="button">.</button>
-          <button type="button" className="opreator">=</button>
+          <button type="button" className="yellow">=</button>
           <br />
         </div>
       </div>
