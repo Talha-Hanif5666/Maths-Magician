b760a51908f1838809a0460980ba71d025e2cc62
Muhammad Talha Hanif <talhahanif5666@gmail.com>
2023-02-21 21:38:07 +0500

    Add quotation js file

---
+++ src/components/quote.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../style.css';
+
+const Quote = () => (
+  <>
+    <section className="qoute">
+      <h1 className="quotetext">
+        &quot;Mathematics is not about numbers, equations, computations, or
+        algorithms: it is about understanding.&quot;
+      </h1>
+      <span className="author">- William Paul Thurston</span>
+    </section>
+  </>
+);
+
+export default Quote;
