7dc45a7b4119d1e2c0ed0cc516a22eec6844db6b
Muhammad Talha Hanif <talhahanif5666@gmail.com>
2023-02-21 21:37:31 +0500

    Update JS file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React, { PureComponent } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import './style.css';
+import Home from './components/homepage';
+import Quote from './components/quote';
+import Links from './components/navbar';
 
 class App extends PureComponent {
   render() {
     return (
-      <Calculator />
+      <>
+        <BrowserRouter>
+          <Links />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Calculator" element={<Calculator />} />
+            <Route path="/quote" element={<Quote />} />
+          </Routes>
+        </BrowserRouter>
+      </>
     );
   }
 }
