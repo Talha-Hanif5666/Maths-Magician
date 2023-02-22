23326944391b54d4bad0b5e15b74f5ab6f501ed1
Muhammad Talha Hanif <talhahanif5666@gmail.com>
2023-02-22 16:58:22 +0500

    Update tests

--- src/logic/calulate.test.js
+++ src/logic/calulate.test.js
@@ -8,4 +8,45 @@ describe('Button 0', () => {
     const result = calculate(op, buttonName);
     expect(result).toEqual(expected);
   });
+
+  describe('Button AC', () => {
+    test('Elemets should be cleared!', () => {
+      const opreations = { total: 10, next: 20, operation: '+' };
+
+      const expected = { total: null, next: null, operation: null };
+      const buttonName = 'AC';
+      const result = calculate(opreations, buttonName);
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('Button =', () => {
+    test('Elements should show result after pressing = sign', () => {
+      const op = { total: 1, next: 2, operation: '+' };
+      const expected = { total: '3', next: null, operation: null };
+      const buttonName = '=';
+      const result = calculate(op, buttonName);
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('Button =', () => {
+    test('Return empty object if = button pressed without opreation', () => {
+      const op = { total: 1, next: 2, operation: null };
+      const expected = {};
+      const buttonName = '=';
+      const result = calculate(op, buttonName);
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('Button +/-', () => {
+    test('Return inverted valeue', () => {
+      const op = { total: '2' };
+      const expected = { total: '-2' };
+      const buttonName = '+/-';
+      const result = calculate(op, buttonName);
+      expect(result).toEqual(expected);
+    });
+  });
 });
--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -5,4 +5,32 @@ describe('Addition', () => {
     const result = operate(4, 7, 'x');
     expect(result).toBe('28');
   });
+
+  describe('Multiplication', () => {
+    test('should Multiply Product 2 and 3 tobe equal 6', () => {
+      const result = operate(2, 3, 'x');
+      expect(result).toBe('6');
+    });
+  });
+
+  describe('Substraction', () => {
+    test('should substract  15 and 30 tobe equal -15', () => {
+      const result = operate(15, 30, '-');
+      expect(result).toBe('-15');
+    });
+  });
+
+  describe('Division', () => {
+    test('should divide  36 by 10 tobe equal 3.6', () => {
+      const result = operate(36, 10, '÷');
+      expect(result).toBe('3.6');
+    });
+  });
+
+  describe('Modulos', () => {
+    test('should find the reminder of 10 modulo 5 tobe equals 0', () => {
+      const result = operate(10, 5, '%');
+      expect(result).toBe('0');
+    });
+  });
 });
